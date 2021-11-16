b995bf32b90dc1369088daab9bf2c596d1989874
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-11-15 18:25:06 -0800

    worked on setting up seed sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,33 @@
-DROP DATABASE IF EXISTS employeeTracker_db;
+DROP DATABASE IF EXISTS employee_tracker_db;
 
-CREATE DATABASE employeeTracker_db;
+CREATE DATABASE employee_tracker_db;
 
-USE employeeTracker_db;
+USE employee_tracker_db;
 
 DROP TABLE IF EXISTS department;
 
-CREATE TABLE department {
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE department(
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   name VARCHAR(30)
-};
+);
 
-DROP TABLE IF EXISTS role; 
+DROP TABLE IF EXISTS role;
 
-CREATE TABLE role {
-
-}
+CREATE TABLE role(
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  title VARCHAR(30),
+  department_id INT,
+  salary DECIMAL(10),
+);
 
 DROP TABLE IF EXISTS employees;
 
-CREATE TABLE employees {
-  id INT NOT NUll AUTO_INCREMENT,
-  firstName VARCHAR(30),
-  lastName VARCHAR(30),
+CREATE TABLE employees (
+  id INT PRIMARY KEY NOT NUll AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
   role_id INT NOT NULL,
   manager_id INT,
   PRIMARY KEY (id) 
-}
+);
 
