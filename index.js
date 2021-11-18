54b57f33270daa21fd4cbc21e70aeb7af81b83bd
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-11-17 21:05:01 -0800

    started working on index.js to return prompt questions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const {prompt} = require("inquirer");
 const logo = require("asciiart-logo");
-const sequelize = require('./db/connection')
+const sequelize = require('./db/connection');
+const { prompt } = require("inquirer");
 
 // load prompts and display text using ASCII-art Logo
 let init = () => {
@@ -8,3 +8,43 @@ let init = () => {
 
   console.log(logoText);
 } 
+
+let mainPrompt = () => {
+  prompt([
+    {
+      message: "What would you like to do?",
+      type: "list",
+      name: "optionChoice",
+      choices: ["View", "Add", "Edit", "Remove", "Quit"]
+    }
+  ]).then(answers => {
+    switch(answers.optionChoice) {
+      case "View":
+        return viewPrompt();
+      case "Add":
+        return createPrompt();
+      case "Edit":
+        return updatePrompt();
+      case "Remove":
+        return removePrompt();
+      case "Quit":
+        return quitPrompt();
+    }
+  });
+}
+
+function viewPrompt() {
+  prompt([
+    {
+      message: "View:",
+      type: "list",
+      name: "table_name",
+      choices: [
+        {name: "All Employees", value: "employees"}, 
+        {name: "All Departments", value: "departments"}, 
+        {name: "All Roles", value: "roles"}]
+    }
+  ]).then(answers => {
+    db.showAll(answers.table_name)
+  });
+}
